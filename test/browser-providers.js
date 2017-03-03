307be9ca5d4b6bc92a9e8aa108e6ae9f5f1ba70e
Tasos Bekos <tbekos@gmail.com>
2017-03-03 14:45:15 +0200

    chore(CI): bump versions, add new Safari, Edge & remove Android

--- test/browser-providers.js
+++ test/browser-providers.js
@@ -2,12 +2,12 @@ module.exports = {
   'SL_Chrome': {
     base: 'SauceLabs',
     browserName: 'chrome',
-    version: '53'
+    version: 'latest'
   },
   'SL_FireFox': {
     base: 'SauceLabs',
     browserName: 'firefox',
-    version: '49'
+    version: 'latest'
   },
   'SL_Safari_9': {
     base: 'SauceLabs',
@@ -15,22 +15,28 @@ module.exports = {
     platform: 'OS X 10.11',
     version: '9'
   },
+  'SL_Safari_10': {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '10'
+  },
   'SL_IE_11': {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 8.1',
     version: '11'
   },
-  'SL_EDGE': {
+  'SL_EDGE_13': {
     base: 'SauceLabs',
     browserName: 'MicrosoftEdge',
     platform: 'Windows 10',
     version: '13.10586'
   },
-  'SL_ANDROID5': {
+  'SL_EDGE_14': {
     base: 'SauceLabs',
-    browserName: 'android',
-    platform: 'Linux',
-    version: '5.1'
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '14.14393'
   },
 };
